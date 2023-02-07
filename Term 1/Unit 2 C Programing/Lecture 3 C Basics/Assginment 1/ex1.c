04c9269af8f401f962743cda92b3afe8ec3588d7
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-02-07 12:57:03 +0200

    EX 1 Solution

---
+++ Term 1/Unit 2 C Programing/Lecture 3 C Basics/Assginment 1/ex1.c
@@ -0,0 +1,13 @@
+/**
+ * Write C Program to Print a Sentence
+ * ##########Console-output###
+ * C Programming
+ * ###########################
+ */
+
+#include <stdio.h>
+int main()
+{
+    printf("##########Console-output###\nC Programming\n###########################\n");
+    return 0;
+}
