1cba2e853cf558707500a92f0bab95e328d01a5e
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-02-07 15:25:12 +0200

    EX 5 Solution

---
+++ Term 1/Unit 2 C Programing/Lecture 3 C Basics/Assignment 2/ex5.c
@@ -0,0 +1,18 @@
+/**
+ * @author [Ahmed Shehatah]
+ * @email [ahmed2002.eg@mail.com]
+ * @create date 2023-02-07 15:14:50
+ * @modify date 2023-02-07 15:14:50
+ * @desc EX 4 is number pos, neg, or zeero
+ */
+#include <stdio.h>
+int main()
+{
+    printf("Enter Charchter: ");
+    char x;
+    scanf("%c", &x);
+    if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+        printf("%c is alphabet", x);
+    else
+        printf("%c is not alphabet", x);
+}
