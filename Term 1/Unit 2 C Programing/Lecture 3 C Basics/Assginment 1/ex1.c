6cf36f3a3d2374ce4eb9ca903e982e2d0cc832f0
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-02-07 14:09:02 +0200

    Update ex1 output

--- Term 1/Unit 2 C Programing/Lecture 3 C Basics/Assginment 1/ex1.c
+++ Term 1/Unit 2 C Programing/Lecture 3 C Basics/Assginment 1/ex1.c
@@ -1,13 +1,10 @@
 /**
  * Write C Program to Print a Sentence
- * ##########Console-output###
- * C Programming
- * ###########################
  */
 
 #include <stdio.h>
 int main()
 {
-    printf("##########Console-output###\nC Programming\n###########################\n");
+    printf("C Programming\n");
     return 0;
 }
