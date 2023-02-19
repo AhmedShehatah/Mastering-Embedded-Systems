842c5db8c9f09524c8d6eaa84ddf4dcc3bf20a70
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-02-19 16:21:31 +0200

    EX 2 Edit Desc

--- Term 1/Unit 2 C Programing/Lecture 4 C Arrays/Strings Assignment/ex2.c
+++ Term 1/Unit 2 C Programing/Lecture 4 C Arrays/Strings Assignment/ex2.c
@@ -3,7 +3,7 @@
  * @email [ahmed2002.eg@mail.com]
  * @create date 2023-02-19 15:09:00
  * @modify date 2023-02-19 15:09:00
- * @desc [EX 1 Frequency of char]
+ * @desc [EX 2 Length of string]
  */
 #include <stdio.h>
 int main()
