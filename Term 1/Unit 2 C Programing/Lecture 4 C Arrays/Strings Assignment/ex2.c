4509f1bff75c42e9618dc9cece120ab1ee3e5299
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-02-19 16:15:29 +0200

    EX 2 Solution

---
+++ Term 1/Unit 2 C Programing/Lecture 4 C Arrays/Strings Assignment/ex2.c
@@ -0,0 +1,18 @@
+/**
+ * @author [Ahmed Shehatah]
+ * @email [ahmed2002.eg@mail.com]
+ * @create date 2023-02-19 15:09:00
+ * @modify date 2023-02-19 15:09:00
+ * @desc [EX 1 Frequency of char]
+ */
+#include <stdio.h>
+int main()
+{
+    char s[100];
+    printf("Enter String: ");
+    gets(s);
+    int i = 0;
+    while (s[i++] != '\0')
+        ;
+    printf("Length of String: %d\r\n", i - 1);
+}
